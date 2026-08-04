299b184e17ec793a70a71a4463616e2fc468c268
ayman <aymanmohamed1142001@gmail.com>
2026-08-05 02:01:01 +0300

    best seller list view done

--- lib/features/home/presntation/views/home_view_body.dart
+++ lib/features/home/presntation/views/home_view_body.dart
@@ -3,6 +3,7 @@ import 'package:app/constant.dart';
 import 'package:app/core/utils/assets.dart';
 import 'package:app/core/utils/styles.dart';
 import 'package:app/features/home/presntation/views/widgets/Featuerd_ListView_Item.dart';
+import 'package:app/features/home/presntation/views/widgets/besr_seller_list_view.dart';
 import 'package:app/features/home/presntation/views/widgets/best_list_view_item.dart';
 import 'package:app/features/home/presntation/views/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
@@ -12,30 +13,48 @@ class homeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-         CustomAppBar(),
-         FeatuerdListViewItem(),
-         const SizedBox(
-          height: 40,
-         ),
-         Text('Best Seller ',style: Styles.textstyle18.copyWith(
-          fontFamily: kGtSectraFine,
-         ),),
-         SizedBox(
-          height: 20,
-         ),
-         BestListViewItem(),
-      
-        ],
-      ),
+
+
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child:Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+             Padding(
+               padding: const EdgeInsets.symmetric(horizontal: 20),
+               child: CustomAppBar(),
+             ),
+             FeatuerdListViewItem(),
+             const SizedBox(
+              height: 40,
+             ),
+             Padding(
+               padding: const EdgeInsets.symmetric(horizontal: 17),
+               child: Text('Best Seller ',style: Styles.textstyle18.copyWith(
+                fontFamily: kGtSectraFine,
+               ),),
+             ),
+             SizedBox(
+              height: 20,
+             ), 
+            ],
+          ),
+        ),
+     const SliverFillRemaining(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 17),
+          child: BestSellerListView(),
+        ),
+       ), 
+      ],
     );
   }
 
 }
+
+
+
 
 
 
