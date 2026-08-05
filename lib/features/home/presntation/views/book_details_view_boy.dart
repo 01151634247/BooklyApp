01f3c852b45edfa11a5c10ba21c57dac8c2b01d1
ayman <aymanmohamed1142001@gmail.com>
2026-08-05 14:58:05 +0300

    add custom

--- lib/features/home/presntation/views/book_details_view_boy.dart
+++ lib/features/home/presntation/views/book_details_view_boy.dart
@@ -1,5 +1,7 @@
 
+import 'package:app/features/home/presntation/views/widgets/Featuerd_ListView_Item.dart';
 import 'package:app/features/home/presntation/views/widgets/custom_book_detail_appbar.dart';
+import 'package:app/features/home/presntation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -7,11 +9,22 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width =MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25),
       child: Column(
         children: [
-          SafeArea(child: CustomBookDetailAppbar()),
+          SafeArea(child: 
+          CustomBookDetailAppbar()
+          
+          ),
+
+          Padding(
+            padding:  EdgeInsets.symmetric(horizontal: width *.1),
+            child: CustomListViewItem(),
+          ),
+
+
       
         ],
       ),
