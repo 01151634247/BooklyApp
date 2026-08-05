15647a117efede01cf7c041dab2632e467a3289d
ayman <aymanmohamed1142001@gmail.com>
2026-08-05 18:57:25 +0300

    search view done

--- lib/features/search/presentaition/views/widgets/search_view_body.dart
+++ lib/features/search/presentaition/views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
 
+import 'package:app/core/utils/styles.dart';
+import 'package:app/features/home/presntation/views/widgets/best_list_view_item.dart';
 import 'package:app/features/search/presentaition/views/widgets/custom_search_textfield.dart';
 import 'package:flutter/material.dart';
 
@@ -10,13 +12,37 @@ class SearchViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomSearchTextField(),
+          const SizedBox(
+            height:15 ,
+          ),
+          Text('Search Result',style: Styles.textstyle16.copyWith(
+            fontWeight: FontWeight.bold,
+          ),),
+          Expanded(child: SearchResultListview()),
       
       
       
         ],
       ),
     );
+  }
+}
+class SearchResultListview extends StatelessWidget {
+  const SearchResultListview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context,index){
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical:10),
+        child: BestListViewItem(),
+      );
+    });
   }
 }
