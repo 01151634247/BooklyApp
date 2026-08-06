02926bda947a613e41ff5e3258b592d245781438
ayman <aymanmohamed1142001@gmail.com>
2026-08-06 16:10:00 +0300

    add home repo class and either package to handle error

---
+++ lib/features/home/Domain/entities/Book_Entitys.dart
@@ -0,0 +1,11 @@
+class BookEntitys {
+  final String Image;
+   final String title ;
+    final String authorName;
+     final num price;
+     final num rating;
+
+  BookEntitys({required this.Image, required this.title, required this.authorName, required this.price, required this.rating});
+
+  
+}
---
+++ lib/features/home/Domain/repo/home_repo.dart
@@ -0,0 +1,11 @@
+import 'package:app/core/error/failure.dart';
+import 'package:app/features/home/Domain/entities/Book_Entitys.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure,List<BookEntitys>>>fetchFutureBooks();
+  Future<List<BookEntitys>>fetchNewstBooks();
+
+
+
+}
